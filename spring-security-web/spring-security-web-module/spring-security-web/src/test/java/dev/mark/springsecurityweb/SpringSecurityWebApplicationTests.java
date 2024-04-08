c2d74c946a8fdf1eedc63565d15f3649be3192a0
thisIsSirMark <145762444+thisIsSirMark@users.noreply.github.com>
2024-04-08 09:33:52 +0800

    Add files via upload

---
+++ spring-security-web/spring-security-web-module/spring-security-web/src/test/java/dev/mark/springsecurityweb/SpringSecurityWebApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mark.springsecurityweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
